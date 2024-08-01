e4bb48f4104eeafde6a0b530758fe4311d91cd64
Kent <kentsday@outlook.com>
2024-08-01 14:27:20 +0800

    update testnet config

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -4,8 +4,9 @@ yarn install --frozen-lockfile
 
 export VITE_HIBIT_ID_API=https://testnetidapi.hibit.app/
 export VITE_HIBIT_AUTH_SERVER=https://testnetauth.hibit.app/
-export VITE_HIBIT_AUTH_CLIENT_ID=hibit_id_local
-export VITE_TELEGRAM_BOT_ID=6944468360
+export VITE_HIBIT_AUTH_CLIENT_ID=IdServer_HibitIdWeb
+export VITE_EX3_API=https://alphaapi.ex3.one/
+export VITE_TELEGRAM_BOT_ID=7359440424
 export VITE_APP_ENV=TestNet
 
 yarn build:wallet
