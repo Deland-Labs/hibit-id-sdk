07c8216aff724bbc2222dfc42c4114955b721fba
Kent <92845901+kentsday@users.noreply.github.com>
2024-08-01 14:51:09 +0800

    fix: missing env (#72)

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -2,6 +2,8 @@
 
 yarn install --frozen-lockfile
 
+cd apps/wallet/ || exit
+
 export VITE_HIBIT_ID_API=https://testnetidapi.hibit.app/
 export VITE_HIBIT_AUTH_SERVER=https://testnetauth.hibit.app/
 export VITE_HIBIT_AUTH_CLIENT_ID=IdServer_HibitIdWeb
@@ -9,4 +11,4 @@ export VITE_EX3_API=https://alphaapi.ex3.one/
 export VITE_TELEGRAM_BOT_ID=7359440424
 export VITE_APP_ENV=TestNet
 
-yarn build:wallet
+yarn build
