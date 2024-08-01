a415b860daaa196d5a016a8d104a3646d5b1c32f
Rustin <rustinchi.alpha@gmail.com>
2024-08-01 17:59:54 +0800

    🐛 fix: wallet build env

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 yarn install --frozen-lockfile
+
+export VITE_HIBIT_ID_API=https://testnetidapi.hibit.app/
+export VITE_HIBIT_AUTH_SERVER=https://testnetauth.hibit.app/
+export VITE_HIBIT_AUTH_CLIENT_ID=IdServer_HibitIdWeb
+export VITE_EX3_API=https://alphaapi.ex3.one/
+export VITE_TELEGRAM_BOT_ID=7359440424
+export VITE_APP_ENV=TestNet
+
 yarn build:wallet
