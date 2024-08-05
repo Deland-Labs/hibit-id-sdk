cc551ae43928c32fcb8e06c8f325e1de900b035c
Kent <kentsday@outlook.com>
2024-08-05 22:28:55 +0800

    fix: log out issue

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -4,6 +4,7 @@ yarn install --frozen-lockfile
 
 export VITE_HIBIT_ID_API=https://testnetidapi.hibit.app/
 export VITE_HIBIT_AUTH_SERVER=https://testnetauth.hibit.app/
+export VITE_HIBIT_ID_WEB=https://idtestnet.hibit.app/
 export VITE_HIBIT_AUTH_CLIENT_ID=IdServer_HibitIdWeb
 export VITE_EX3_API=https://alphaapi.ex3.one/
 export VITE_TELEGRAM_BOT_ID=7359440424
