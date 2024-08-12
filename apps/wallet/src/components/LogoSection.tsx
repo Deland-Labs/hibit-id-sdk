8e0303e8cfff019bc341b92f3a016775ee793584
Rustin <rustinchi.alpha@gmail.com>
2024-08-12 16:17:39 +0800

    ✨ feat: update brand pic

--- apps/wallet/src/components/LogoSection.tsx
+++ apps/wallet/src/components/LogoSection.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
-import SvgLogo from '../assets/logo.svg?react'
 
 const LogoSection: FC = () => {
   return (
     <section className="flex-1 flex flex-col justify-center items-center">
-      <div className="flex justify-center items-center size-20 rounded-full [background:linear-gradient(180deg,#16D6FF_0%,#0099E6_100%)]">
+      <img src="/brand@2x.png" alt="Brand" className="w-[160px] h-[102px]" />
+      {/* <div className="flex justify-center items-center size-20 rounded-full [background:linear-gradient(180deg,#16D6FF_0%,#0099E6_100%)]">
         <SvgLogo className="size-9" />
-      </div>
+      </div> */}
       <h1 className="mt-2 text-neutral">Hibit ID</h1>
       <p className="mt-2 font-bold text-xl">
         Link <span className="text-primary">Web2</span> to <span className="text-primary">Web3</span> world
