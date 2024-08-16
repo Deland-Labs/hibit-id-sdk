b77e9751389820ac451258994d09be86e99ea116
kent <92845901+kentsday@users.noreply.github.com>
2024-08-16 15:29:37 +0800

    chore: add prod configs

--- apps/wallet/src/utils/oidc/index.ts
+++ apps/wallet/src/utils/oidc/index.ts
@@ -1,13 +1,16 @@
 import {createReactOidc} from "./lib/oidc-spa-4.11.1/src/react";
+import {defaultScopes} from "./hash.ts";
 
 export const {OidcProvider, useOidc, prOidc} = createReactOidc({
-  issuerUri: import.meta.env.VITE_HIBIT_AUTH_SERVER,
-  clientId: import.meta.env.VITE_HIBIT_AUTH_CLIENT_ID,
-  /**
-   * Vite:  `publicUrl: import.meta.env.BASE_URL`
-   * CRA:   `publicUrl: process.env.PUBLIC_URL`
-   * Other: `publicUrl: "/"` (Usually)
-   */
-  scopes: ["openid", "profile", "email", "offline_access"],
-  publicUrl: import.meta.env.BASE_URL,
+    issuerUri: import.meta.env.VITE_HIBIT_AUTH_SERVER,
+    clientId: import.meta.env.VITE_HIBIT_AUTH_CLIENT_ID,
+    /**
+     * Vite:  `publicUrl: import.meta.env.BASE_URL`
+     * CRA:   `publicUrl: process.env.PUBLIC_URL`
+     * Other: `publicUrl: "/"` (Usually)
+     */
+    scopes: defaultScopes,
+    publicUrl: import.meta.env.BASE_URL,
 });
+
+
