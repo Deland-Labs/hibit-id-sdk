f3646d7b4e0f20598d471f5d8e083c8269c956a3
Kent <92845901+kentsday@users.noreply.github.com>
2024-09-04 22:57:46 +0800

    chore: update tg bot config (#159)

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -6,7 +6,8 @@ export VITE_HIBIT_ID_API=https://testnetidapi.hibit.app/
 export VITE_HIBIT_AUTH_SERVER=https://testnetauth.hibit.app/
 export VITE_HIBIT_AUTH_CLIENT_ID=IdServer_HibitIdWeb
 export VITE_EX3_API=https://alphaapi.ex3.one/
-export VITE_TELEGRAM_BOT_ID=7359440424
+# hibit_stage_bot
+export VITE_TELEGRAM_BOT_ID=7330074911
 export VITE_APP_ENV=TestNet
 
 yarn build:wallet
