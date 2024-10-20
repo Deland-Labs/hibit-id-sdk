499cfb85974b7df2bf2a14f0d4975ac92bd269b3
Kent <92845901+kentsday@users.noreply.github.com>
2024-10-20 22:50:38 +0800

    feat: sign test and chainassetType update (#176)

--- apps/wallet/src/utils/basicTypes.ts
+++ apps/wallet/src/utils/basicTypes.ts
@@ -319,8 +319,8 @@ export class ChainAssetType {
   static NativeGas = new ChainAssetType(new BigNumber(1));
   static ERC20 = new ChainAssetType(new BigNumber(3));
   static ERC721 = new ChainAssetType(new BigNumber(4));
-  static DFT = new ChainAssetType(new BigNumber(5));
-  static ICRC1 = new ChainAssetType(new BigNumber(6));
+  static ICP = new ChainAssetType(new BigNumber(5));
+  static ICRC3 = new ChainAssetType(new BigNumber(6));
   static BRC20 = new ChainAssetType(new BigNumber(7));
   static SPL = new ChainAssetType(new BigNumber(8));
   static Jetton = new ChainAssetType(new BigNumber(10));
