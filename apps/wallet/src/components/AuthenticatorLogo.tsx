7bf2cdb8fb8e2fc70d409db26625d82050c16aa2
rustin01 <141540002+rustin01@users.noreply.github.com>
2024-10-23 10:20:49 +0800

    ✨ feat: add login entry for Google and X (#182)

--- apps/wallet/src/components/AuthenticatorLogo.tsx
+++ apps/wallet/src/components/AuthenticatorLogo.tsx
@@ -2,6 +2,8 @@ import { FC, SVGProps } from "react";
 import { AuthenticatorType } from "@delandlabs/hibit-id-sdk";
 import UnknownSvg from '../assets/auth-logos/UNKNOWN.svg?react';
 import TelegramSvg from '../assets/auth-logos/Telegram.svg?react';
+import GoogleSvg from '../assets/auth-logos/Google.svg?react'
+import XSvg from '../assets/auth-logos/X.svg?react'
 
 const AuthenticatorLogo: FC<SVGProps<SVGSVGElement> & {
   title?: string;
@@ -10,6 +12,12 @@ const AuthenticatorLogo: FC<SVGProps<SVGSVGElement> & {
   switch (type) {
     case AuthenticatorType.Telegram: {
       return <TelegramSvg {...props} />
+    }
+    case AuthenticatorType.Google: {
+      return <GoogleSvg {...props} />
+    }
+    case AuthenticatorType.X: {
+      return <XSvg {...props} />
     }
     default: {
       return <UnknownSvg {...props} />
---
+++ apps/wallet/src/utils/auth/providers/x.ts
@@ -0,0 +1,14 @@
+import { IAuthenticateProvider } from "../types";
+import { AuthenticatorType } from "@delandlabs/hibit-id-sdk";
+
+const AUTH_SERVER_URL = import.meta.env.VITE_HIBIT_AUTH_SERVER
+
+export class XAuthenticateProvider implements IAuthenticateProvider {
+  public readonly type = AuthenticatorType.X
+  
+  public authenticate: (launchParams?: any) => Promise<any> = async (launchParams?: string) => {
+    window.location.href = 
+      `${AUTH_SERVER_URL}id/login/x?returnUrl=${encodeURIComponent(`${location.origin}/oidc-login`)}`
+    return
+  }
+}
