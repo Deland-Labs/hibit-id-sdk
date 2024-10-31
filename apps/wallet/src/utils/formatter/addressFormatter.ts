134d46542a86890f379607f29a86578507c1decd
rustin01 <141540002+rustin01@users.noreply.github.com>
2024-10-31 16:51:59 +0800

    ✨ feat: compress rule for dfinity address (#195)

--- apps/wallet/src/utils/formatter/addressFormatter.ts
+++ apps/wallet/src/utils/formatter/addressFormatter.ts
@@ -1,4 +1,10 @@
-export const formatAddress = (walletAddress: string) =>
-  walletAddress?.length > 11
+import { Chain } from "../basicTypes";
+
+export const formatAddress = (walletAddress: string, chainType?: Chain): string => {
+  if (chainType?.equals(Chain.Dfinity)) {
+    return walletAddress.slice(0, 5) + '...' + walletAddress.slice(-3)
+  }
+  return walletAddress?.length > 11
     ? walletAddress.slice(0, 5) + '...' + walletAddress.slice(-4)
     : walletAddress || '';
+}
