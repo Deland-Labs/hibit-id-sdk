c29f2a88e01bf87d0375d932fc49e51d1823c94b
Witter <varik1770@gmail.com>
2024-11-30 23:22:42 +0800

    feat: tx script builder

--- packages/coin-kaspa-rpc/src/lib/Address/Version.ts
+++ packages/coin-kaspa-rpc/src/lib/Address/Version.ts
@@ -1,10 +1,22 @@
+/**
+ * Enum representing different address versions.
+ */
 enum AddressVersion {
   PubKey = 0,
   PubKeyECDSA = 1,
-  ScriptHash = 8,
+  ScriptHash = 8
 }
 
+/**
+ * Helper class for AddressVersion related operations.
+ */
 class AddressVersionHelper {
+  /**
+   * Returns the length of the public key for a given address version.
+   * @param {AddressVersion} version - The address version.
+   * @returns {number} The length of the public key.
+   * @throws Will throw an error if the version is unknown.
+   */
   public static publicKeyLen(version: AddressVersion): number {
     switch (version) {
       case AddressVersion.PubKey:
