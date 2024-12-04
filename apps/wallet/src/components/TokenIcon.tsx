89e3bacf9a6cb1c4fdf2e57981846a0513c2c2af
Rustin <rustinchi.alpha@gmail.com>
2024-12-04 17:38:15 +0800

    ✨ feat: support AIA token icon

--- apps/wallet/src/components/TokenIcon.tsx
+++ apps/wallet/src/components/TokenIcon.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { RootAssetInfo } from '../apis/models';
 import { twMerge } from 'tailwind-merge';
 
@@ -15,23 +15,28 @@ const TokenIcon: FC<TokenIconProps> = ({
   onlyIcon = false,
   hideName = false,
 }) => {
-  let srcUrl = token?.icon || '';
-  if (!srcUrl && token?.assetSymbol) {
+  const [srcIndex, setSrcIndex] = useState(0)
+
+  let srcUrlArr: string[] = token?.icon ? [token?.icon] : [];
+  if (!srcUrlArr.length && token?.assetSymbol) {
     const symbol = encodeURIComponent(token.assetSymbol);
-    srcUrl =
-      `/token-icons/${symbol}.svg` ||
-      `/token-icons-png/${symbol}.png`;
+    srcUrlArr = ['svg', 'webp', 'png', 'jpeg', 'jpg'].map((ext: string) => `/token-icons/${symbol}.${ext}`);
   }
 
   return (
     <div className="flex items-center gap-2">
       <img
-        src={srcUrl}
+        src={srcUrlArr[srcIndex]}
         alt={token?.assetSymbol}
         className={twMerge('rounded-full', size === 'sm' && 'size-6', size === 'md' && 'size-8', size === 'lg' && 'size-12')}
-        onError={ev =>
-          ((ev.target as HTMLImageElement).src = '/token-icons/UNKNOWN.svg')
-        }
+        onError={ev => {
+          if (srcIndex < srcUrlArr.length - 1) {
+            (ev.target as HTMLImageElement).src = srcUrlArr[srcIndex + 1]
+            setSrcIndex(srcIndex + 1)
+          } else {
+            (ev.target as HTMLImageElement).src = '/token-icons/UNKNOWN.svg'
+          }
+        }}
       />
       {!onlyIcon && (
         <div className='flex flex-col'>
