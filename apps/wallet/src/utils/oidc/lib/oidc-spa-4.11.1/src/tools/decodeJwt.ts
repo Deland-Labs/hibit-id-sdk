19000825b47a667807a7e4f47df95296cd72fcd1
Rustin <rustinchi.alpha@gmail.com>
2024-12-16 11:33:44 +0800

    ♻️ refactor: remove app related code

--- apps/wallet/src/utils/oidc/lib/oidc-spa-4.11.1/src/tools/decodeJwt.ts
+++
@@ -1,2 +0,0 @@
-import { jwt_decode } from "../vendor/oidc-client-ts-and-jwt-decode";
-export const decodeJwt = jwt_decode;
--- apps/wallet/src/utils/oidc/lib/oidc-spa-4.11.1/src/vendor/oidc-client-ts-and-jwt-decode.ts
+++
@@ -1,6 +0,0 @@
-export * from "oidc-client-ts";
-
-import * as jwt_decode_ns from "jwt-decode";
-export { jwt_decode_ns };
-import { jwtDecode } from "jwt-decode";
-export { jwtDecode as jwt_decode };
