63829952eab97ff6ff30025791267a96990faa5c
Witter <varik1770@gmail.com>
2024-12-17 12:18:48 +0800

    chore: fix update to node22

--- packages/crypto-lib/src/base/base58.ts
+++ packages/crypto-lib/src/base/base58.ts
@@ -1,11 +1,11 @@
-import { base58 } from "@scure/base"
-export function toBase58(data: Uint8Array | Buffer | Number[]): string {
-    const a = Buffer.from(data)
-    return base58.encode(Uint8Array.from(a))
+import { base58 } from '@scure/base';
+
+export function toBase58(data: Uint8Array | Buffer | number[]): string {
+  return base58.encode(Uint8Array.from(data));
 }
 
 export function fromBase58(data: string): Uint8Array {
-    return base58.decode(data)
+  return base58.decode(data);
 }
 
-export {base58}
+export { base58 };
